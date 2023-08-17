cfdb9015d171d0cfbecdcfa1fcdc3f3438936554
Innocent11111 <2872128114@qq.com>
2023-08-17 18:09:53 +0800

    整合

--- ChatRoom/ChatRoom.h
+++ ChatRoom/ChatRoom.h
@@ -22,3 +22,143 @@ private slots:
 private:
     Ui::ChatRoomClass ui;
 };
+
+// 自定义的数据模型类
+//class ChatModel : public QAbstractListModel {
+//    Q_OBJECT
+//public:
+//    // 聊天消息结构体
+//    struct ChatMessage {
+//        QString sender;
+//        QString message;
+//        QPixmap avatar;
+//    };
+//
+//    explicit ChatModel(QObject* parent = nullptr)
+//        : QAbstractListModel(parent) {}
+//
+//    int rowCount(const QModelIndex& parent = QModelIndex()) const override {
+//        Q_UNUSED(parent)
+//            return m_messages.size();
+//    }
+//
+//    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override {
+//        if (!index.isValid() || index.row() >= m_messages.size()) {
+//            return QVariant();
+//        }
+//
+//        if (role == Qt::UserRole) {
+//            return QVariant::fromValue(m_messages[index.row()]);
+//        }
+//
+//        return QVariant();
+//    }
+//
+//    void addMessage(const ChatMessage& message) {
+//        beginInsertRows(QModelIndex(), m_messages.size(), m_messages.size());
+//        m_messages.append(message);
+//        endInsertRows();
+//    }
+//
+//private:
+//    QVector<ChatMessage> m_messages;
+//};
+//
+//// 自定义的委托类
+//class ChatDelegate : public QStyledItemDelegate {
+//    Q_OBJECT
+//public:
+//    explicit ChatDelegate(QObject* parent = nullptr)
+//        : QStyledItemDelegate(parent) {}
+//
+//    QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const override {
+//        Q_UNUSED(option)
+//
+//            if (!index.isValid()) {
+//                return QStyledItemDelegate::sizeHint(option, index);
+//            }
+//
+//        const ChatModel::ChatMessage message = index.data(Qt::UserRole).value<ChatModel::ChatMessage>();
+//        QPixmap avatar = message.avatar;
+//
+//        // 根据需求设置头像大小和边距
+//        int avatarSize = 40;
+//        int margin = 10;
+//
+//        return QSize(avatarSize + margin * 2, qMax(avatarSize, option.fontMetrics.height()) + margin * 2);
+//    }
+//
+//    void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const override {
+//        if (!index.isValid()) {
+//            QStyledItemDelegate::paint(painter, option, index);
+//            return;
+//        }
+//
+//        const int avatarSize = 40;
+//        const int margin = 10;
+//        const int padding = 5;
+//        const ChatModel::ChatMessage message = index.data(Qt::UserRole).value<ChatModel::ChatMessage>();
+//        QPixmap avatar = message.avatar;
+//
+//        QRect avatarRect(option.rect.topLeft() + QPoint(margin, margin), QSize(avatarSize, avatarSize));
+//        QRect textRect(option.rect.topLeft() + QPoint(margin + avatarSize + padding, margin),
+//            QSize(option.rect.width() - margin * 2 - avatarSize - padding * 2, option.rect.height() - margin * 2));
+//
+//        // 绘制头像
+//        painter->drawPixmap(avatarRect, avatar.scaled(avatarRect.size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+//
+//        // 绘制消息文本
+//        painter->drawText(textRect, Qt::TextWordWrap | Qt::AlignLeft | Qt::AlignTop, message.message);
+//    }
+//};
+//
+//// 主窗口类
+//class MainWindow : public QMainWindow {
+//    Q_OBJECT
+//public:
+//    explicit MainWindow(QWidget* parent = nullptr)
+//        : QMainWindow(parent) {
+//        // 初始化数据模型
+//        m_chatModel = new ChatModel(this);
+//
+//        // 初始化列表视图
+//        m_listView = new QListView(this);
+//        m_listView->setModel(m_chatModel);
+//
+//        // 使用自定义委托
+//        m_chatDelegate = new ChatDelegate(this);
+//        m_listView->setItemDelegate(m_chatDelegate);
+//
+//        // 设置布局
+//        QVBoxLayout* layout = new QVBoxLayout;
+//        layout->addWidget(m_listView);
+//
+//        QWidget* centralWidget = new QWidget(this);
+//        centralWidget->setLayout(layout);
+//        setCentralWidget(centralWidget);
+//    }
+//
+//public slots:
+//    // 发送按钮的点击事件槽函数
+//    void on_sendButton_clicked() {
+//        QString sender = "Sender";
+//        QString message = ui->lineEdit->text();
+//        QPixmap avatar(":/path/to/avatar.png");
+//
+//        ChatModel::ChatMessage chatMessage;
+//        chatMessage.sender = sender;
+//        chatMessage.message = message;
+//        chatMessage.avatar = avatar;
+//
+//        // 添加聊天消息到数据模型
+//        m_chatModel->addMessage(chatMessage);
+//
+//        // 清空输入框
+//        ui->lineEdit->clear();
+//    }
+//
+//private:
+//    ChatModel* m_chatModel;
+//    QListView* m_listView;
+//    ChatDelegate* m_chatDelegate;
+//};
--- ChatRoom/x64/Debug/uic/ui_NewContacts.h
+++ ChatRoom/x64/Debug/uic/ui_NewContacts.h
@@ -10,11 +10,10 @@
 #define UI_NEWCONTACTS_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,28 +21,34 @@ QT_BEGIN_NAMESPACE
 class Ui_NewContactsClass
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
     QWidget *centralWidget;
-    QStatusBar *statusBar;
+    QPushButton *Return;
 
     void setupUi(QMainWindow *NewContactsClass)
     {
         if (NewContactsClass->objectName().isEmpty())
             NewContactsClass->setObjectName(QString::fromUtf8("NewContactsClass"));
-        NewContactsClass->resize(600, 400);
-        menuBar = new QMenuBar(NewContactsClass);
-        menuBar->setObjectName(QString::fromUtf8("menuBar"));
-        NewContactsClass->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(NewContactsClass);
-        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
-        NewContactsClass->addToolBar(mainToolBar);
+        NewContactsClass->resize(750, 460);
+        NewContactsClass->setMinimumSize(QSize(750, 460));
+        NewContactsClass->setMaximumSize(QSize(750, 460));
         centralWidget = new QWidget(NewContactsClass);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        Return = new QPushButton(centralWidget);
+        Return->setObjectName(QString::fromUtf8("Return"));
+        Return->setGeometry(QRect(10, 10, 121, 51));
+        Return->setMinimumSize(QSize(70, 40));
+        Return->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    text-align: left;\n"
+"}\n"
+"\n"
+"QPushButton::icon {\n"
+"    margin-left: 0px;  // \345\260\206\345\233\276\346\240\207\347\232\204\345\267\246\350\276\271\350\267\235\350\256\276\344\270\2720\n"
+"}"));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8("../../\345\233\276\347\211\207/\350\277\224\345\233\236\345\217\214\347\256\255\345\244\264.png"), QSize(), QIcon::Normal, QIcon::Off);
+        Return->setIcon(icon);
+        Return->setIconSize(QSize(50, 50));
         NewContactsClass->setCentralWidget(centralWidget);
-        statusBar = new QStatusBar(NewContactsClass);
-        statusBar->setObjectName(QString::fromUtf8("statusBar"));
-        NewContactsClass->setStatusBar(statusBar);
 
         retranslateUi(NewContactsClass);
 
@@ -53,6 +58,7 @@ public:
     void retranslateUi(QMainWindow *NewContactsClass)
     {
         NewContactsClass->setWindowTitle(QCoreApplication::translate("NewContactsClass", "NewContacts", nullptr));
+        Return->setText(QString());
     } // retranslateUi
 
 };
