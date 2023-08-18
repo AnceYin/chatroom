de7ce924ab073b84f167ae91d5f70a8c98bdf44e
诺诺 <570951613@qq.com>
2023-08-18 12:51:38 +0800

    v1.0.0的数据库表设计

--- documents/数据库/CreateDatabaseTables.sql
+++ documents/数据库/CreateDatabaseTables.sql
@@ -1,49 +1,34 @@
--- 创建用户表
+-- 用户ID规定5位数初始ID为10000，群聊ID规定8位数初始ID为10000000
+
+-- 创建Users表
 CREATE TABLE Users (
-user_id INT AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(255) NOT NULL,
-password VARCHAR(255) NOT NULL
+  user_id INT(5) AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  ip VARCHAR(50),
+  port INT,
+  UNIQUE KEY unique_username (username)
 );
--- 创建好友关系表
-CREATE TABLE Friends (
-friend_id INT AUTO_INCREMENT PRIMARY KEY,
-user_id INT,
-friend_user_id INT,
-friend_nickname VARCHAR(255) DEFAULT NULL,
-status VARCHAR(20),
-FOREIGN KEY (user_id) REFERENCES Users(user_id),
-FOREIGN KEY (friend_user_id) REFERENCES Users(user_id)
+
+ALTER TABLE Users AUTO_INCREMENT = 10000;
+
+-- 创建Teams表
+CREATE TABLE Teams (
+  team_id INT(8) AUTO_INCREMENT PRIMARY KEY,
+  team_name VARCHAR(50) NOT NULL,
+  creator_id INT(5),
+  FOREIGN KEY (creator_id) REFERENCES Users(user_id)
 );
--- 创建私聊消息记录表
-CREATE TABLE PrivateMessage (
-message_id INT AUTO_INCREMENT PRIMARY KEY,
-sender_id INT,
-receiver_id INT,
-content VARCHAR(255),
-FOREIGN KEY (sender_id) REFERENCES Users(user_id),
-FOREIGN KEY (receiver_id) REFERENCES Users(user_id)
+
+ALTER TABLE Teams AUTO_INCREMENT = 10000000;
+
+-- 创建TeamMember表
+CREATE TABLE TeamMember (
+  team_member_id INT(8) AUTO_INCREMENT PRIMARY KEY,
+  group_id INT(8),
+  member_id INT(5),
+  FOREIGN KEY (group_id) REFERENCES Teams(team_id),
+  FOREIGN KEY (member_id) REFERENCES Users(user_id)
 );
--- 创建群组表
-CREATE TABLE team (
-team_id INT AUTO_INCREMENT PRIMARY KEY,
-team_name VARCHAR(255),
-creator_id INT,
-FOREIGN KEY (creator_id) REFERENCES Users(user_id)
-);
--- 创建群组成员表
-CREATE TABLE TeamMembers (
-team_member_id INT AUTO_INCREMENT PRIMARY KEY,
-team_id INT,
-member_id INT,
-FOREIGN KEY (team_id) REFERENCES team(team_id),
-FOREIGN KEY (member_id) REFERENCES Users(user_id)
-);
--- 创建群聊消息记录表
-CREATE TABLE TeamMessage (
-message_id INT AUTO_INCREMENT PRIMARY KEY,
-sender_id INT,
-team_id INT,
-content VARCHAR(255),
-FOREIGN KEY (sender_id) REFERENCES Users(user_id),
-FOREIGN KEY (team_id) REFERENCES team(team_id)
-);
+
+
